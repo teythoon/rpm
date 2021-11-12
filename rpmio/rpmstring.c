ebb27abd5a2a74e03fa289cb86cc87bea466a847
Justus Winter <justus@sequoia-pgp.org>
2021-11-12 13:50:47 +0100

    Move rpmHexStr to rpmstring.c

--- rpmio/rpmstring.c
+++ rpmio/rpmstring.c
@@ -198,3 +198,18 @@ size_t rstrlcpy(char *dest, const char *src, size_t n)
 
     return s - src - 1; /* count does not include NUL */
 }
+
+char * rpmHexStr(const uint8_t *p, size_t plen)
+{
+    char *t, *str;
+    str = t = xmalloc(plen * 2 + 1);
+    static char const hex[] = "0123456789abcdef";
+    while (plen-- > 0) {
+	size_t i;
+	i = *p++;
+	*t++ = hex[ (i >> 4) & 0xf ];
+	*t++ = hex[ (i     ) & 0xf ];
+    }
+    *t = '\0';
+    return str;
+}
